d525509036c09bfb911394766522a14775615bce
killer1234d <benjamingriller@gmail.com>
2020-09-28 22:21:44 +0200

    Added logger, command executor and plugin manager

--- src/main/java/de/untrusteduser/customweapons/CustomWeapons.java
+++ src/main/java/de/untrusteduser/customweapons/CustomWeapons.java
@@ -1,18 +1,40 @@
 package de.untrusteduser.customweapons;
 
+import de.untrusteduser.customweapons.commands.GiveWeaponCmd;
+import de.untrusteduser.customweapons.weapons.melee.FireRod;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
 
 public final class CustomWeapons extends JavaPlugin
 {
+    public Logger logger;
+    private static CustomWeapons plugin;
+
     @Override
     public void onEnable()
     {
+        plugin = this;
+        logger = this.getLogger();
+        PluginManager pluginManager = Bukkit.getPluginManager();
 
+        logger.info("Enabling Plugin");
+
+        getCommand("cwg").setExecutor(new GiveWeaponCmd());
+
+        pluginManager.registerEvents(new FireRod(), this);
     }
 
     @Override
     public void onDisable()
     {
 
+    }
+
+    public static CustomWeapons getPlugin()
+    {
+        return plugin;
     }
 }
