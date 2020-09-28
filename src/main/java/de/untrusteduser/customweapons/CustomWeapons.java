91e574f94b45470f49c3543f9629d4438f1a1541
killer1234d <benjamingriller@gmail.com>
2020-09-28 17:37:25 +0200

    Initial Commit

---
+++ src/main/java/de/untrusteduser/customweapons/CustomWeapons.java
@@ -0,0 +1,17 @@
+package de.untrusteduser.customweapons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomWeapons extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
